25c9c700c1ef32bef2d0d8d83601128925bc3324
chn555 <chn566@gmail.com>
2017-11-25 12:58:20 +0200

    Update gnome.sh
    added sudo to apt 
    removed dump extensoins

--- gnome.sh
+++ gnome.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #this installs chrome, chrome-gnome-shell, guake and gnome-tweak-tool
 
-apt update
-apt install git chromium-browser chrome-gnome-shell guake gnome-tweak-tool -y
+sudo apt update
+sudo apt install git chromium-browser chrome-gnome-shell guake gnome-tweak-tool -y
 
 
 #this installs dash to panel extension on ubuntu
@@ -11,6 +11,4 @@ sudo wget -O /usr/local/bin/gnomeshell-extension-manage "https://raw.githubuserc
 sudo chmod +x /usr/local/bin/gnomeshell-extension-manage
 gnomeshell-extension-manage --install --extension-id 1160 --version 3.26 --user
 gnomeshell-extension-manage --install --extension-id 779 --version 3.26 --user
-gnomeshell-extension-manage --install --extension-id 885 --version 3.26 --user
-gnomeshell-extension-manage --install --extension-id 517 --version 3.26 --user
 
